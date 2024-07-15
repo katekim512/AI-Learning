da7ba1541dd957fe0987b62f19ac2953a9a39315
jiheunkim <ziheun54@gmail.com>
2024-07-15 15:37:35 +0900

    CRA : eslint 설정

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,10 +1,11 @@
+import { ChakraProvider } from "@chakra-ui/react";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import { RecoilRoot } from "recoil";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { RouterProvider } from "react-router-dom";
-import { ChakraProvider } from "@chakra-ui/react";
+import { RecoilRoot } from "recoil";
+
 import router from "./router";
 
 const root = ReactDOM.createRoot(
