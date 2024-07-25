52a5e94c4a9645f37aac557a860babb7a31dce40
jiheunkim <ziheun54@gmail.com>
2024-07-25 13:40:18 +0900

    [Design 🎨] 모바일 css style에 분리 및 스크롤바 속성 설정

---
+++ frontend/src/style/MobileStyle.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+export const MobileContainer = styled.div`
+  max-width: 400px; // 개발용 화면 디자인 확인
+  width: 100vw;
+  margin-left: auto;
+  margin-right: auto;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+  height: 100vh;
+  overflow-y: auto;
+`
