ac4fde064587ffbc5c3790d9edb6fc18daaef565
jiheunkim <ziheun54@gmail.com>
2024-07-27 01:04:45 +0900

    [Feat 💡] AI 교육여행 버튼 연결

--- frontend/src/pages/CalendarPage/Calendar.tsx
+++ frontend/src/pages/CalendarPage/Calendar.tsx
@@ -1,9 +1,25 @@
+import { FaWandMagicSparkles } from 'react-icons/fa6'
+import { useNavigate } from 'react-router-dom'
+
+import * as L from './styles/Calendar.style'
 import BottomMenuBar from '../../components/BottomMenuBar/BottomMenuBar'
 
 const Calendar = () => {
+  const navigate = useNavigate()
+
+  const handleComplete = () => {
+    navigate('/ai-schedule-step1')
+  }
+
   return (
     <>
-      Calendar Page
+      <L.HeaderSection>
+        Calendar Page
+        <L.AIScheduleButton onClick={handleComplete}>
+          <FaWandMagicSparkles />
+          &nbsp;&nbsp;AI 교육여행
+        </L.AIScheduleButton>
+      </L.HeaderSection>
       <BottomMenuBar />
     </>
   )
