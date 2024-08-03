6f0836bba9e906208b2ed336f352e6c87f396b23
jiheunkim <ziheun54@gmail.com>
2024-08-04 00:24:41 +0900

    [Feat 💡] 일별 일정 로드 기능 1차

--- frontend/src/pages/CalendarPage/components/DateDrawer.tsx
+++ frontend/src/pages/CalendarPage/components/DateDrawer.tsx
@@ -1,18 +1,47 @@
 import PropTypes from 'prop-types'
+import { useState } from 'react'
 
-import * as L from '../styles/Calendar.style'
+import DrawerButton from './DrawerButton'
+import PlaceBox from './PlaceBox'
+import * as L from '../styles/DateDrawer.style'
 
 interface DateDrawerProps {
   date: string
 }
 
+const formatDate = (dateString: string): string => {
+  const strWeak = ['일', '월', '화', '수', '목', '금', '토']
+  const date = new Date(dateString)
+  const month = date.getMonth() + 1
+  const day = date.getDate()
+  const weekDay = strWeak[date.getDay()]
+  return `${month}.${day} / ${weekDay}`
+}
+
 const DateDrawer: React.FC<DateDrawerProps> = ({ date }) => {
+  const [isExpanded, setIsExpanded] = useState(false)
+
+  const toggleHeight = () => {
+    setIsExpanded(!isExpanded)
+  }
+
   return (
     <>
-      <L.DrawerContainer>
+      <L.DrawerContainer isExpanded={isExpanded}>
+        <DrawerButton onToggleHeight={toggleHeight} />
         <L.DrawerHeader>
-          <L.DrawerHeaderText>{date}</L.DrawerHeaderText>
+          <L.DrawerHeaderText>{formatDate(date)}</L.DrawerHeaderText>
+          <L.DrawerHeaderEditText>편집</L.DrawerHeaderEditText>
         </L.DrawerHeader>
+        <L.DrawerCenter isExpanded={isExpanded}>
+          <PlaceBox date={date} />
+        </L.DrawerCenter>
+        <L.DrawerBottom>
+          <L.DrawerBottomBox>
+            <L.DrawerBottomButton>장소 추가</L.DrawerBottomButton>
+            <L.DrawerBottomButton>메모 추가</L.DrawerBottomButton>
+          </L.DrawerBottomBox>
+        </L.DrawerBottom>
       </L.DrawerContainer>
     </>
   )
