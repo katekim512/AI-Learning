7c7850bf85846ea5dd127da4da3b4cba720d6a00
jiheunkim <ziheun54@gmail.com>
2024-08-05 21:23:20 +0900

    [Fix ✏️] 버튼 클릭 잘 안되는 문제 해결

--- frontend/src/components/CloseButton/CloseButton.tsx
+++ frontend/src/components/CloseButton/CloseButton.tsx
@@ -1,18 +1,16 @@
 import { Icon } from '@iconify/react'
 import { useNavigate } from 'react-router-dom'
 
-import { CloseButton } from './styles/CloseButton.style'
+import { CloseButton, Container } from './styles/CloseButton.style'
 
 const BackButton = () => {
   const navigate = useNavigate()
   return (
-    // <Container>
-    <CloseButton onClick={() => navigate(-1)}>
-      {/* <StyledIcon> */}
-      <Icon icon='ei:close' />
-      {/* </StyledIcon> */}
-    </CloseButton>
-    // </Container>
+    <Container>
+      <CloseButton onClick={() => navigate(-1)}>
+        <Icon icon='ei:close' />
+      </CloseButton>
+    </Container>
   )
 }
 
