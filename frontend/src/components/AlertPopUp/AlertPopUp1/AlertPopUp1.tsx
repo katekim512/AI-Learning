7ca80b9c9f7704cbdef3878c1739e51a84e3d279
YeeunKim <katekim512@konkuk.ac.kr>
2024-08-10 17:02:56 +0900

    [Refactoring ⚙️] 알림팝업 style 분리

--- frontend/src/components/AlertPopUp/AlertPopUp1/AlertPopUp1.tsx
+++ frontend/src/components/AlertPopUp/AlertPopUp1/AlertPopUp1.tsx
@@ -1,64 +1,6 @@
 import React from 'react'
-import styled from 'styled-components'
+import * as L from './AlerPopUp1.styles/AlertPopUp1.style'
 
-const Overlay = styled.div`
-  position: fixed;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  background-color: rgba(0, 0, 0, 0.5); /* 다크 배경 */
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  z-index: 999;
-`
-
-const ModalContainer = styled.div`
-  background-color: white;
-  border-radius: 10px; /* 둥근 모서리 */
-  width: 278px;
-  padding: 5px;
-  text-align: center;
-  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
-`
-
-const Title = styled.h2`
-  margin: 0;
-  margin-bottom: 10px;
-  margin-top: 15px;
-  font-size: 18px;
-  font-weight: bold;
-`
-
-const Message = styled.p`
-  margin: 0;
-  margin-top: 30px;
-  margin-bottom: 30px;
-  font-size: 14px;
-`
-
-const Divider = styled.hr`
-  border: none;
-  border-top: 1px solid #525fd4;
-  margin: 0;
-  margin-bottom: 5px;
-`
-
-const ConfirmButton = styled.button`
-  background-color: transparent;
-  color: black;
-  border: none;
-  border-radius: 5px;
-  padding: 10px 20px;
-  cursor: pointer;
-  font-size: 14px;
-  width: 100%;
-
-  &:hover {
-    background-color: #0056b3;
-  }
-`
 // 1. 메세지와 2. 확인 버튼 눌렀을 때 실행 함수 넣으면 작동
 // Props 타입 정의
 interface AlertPopUp1Props {
@@ -68,14 +10,14 @@ interface AlertPopUp1Props {
 
 const AlertPopUp1: React.FC<AlertPopUp1Props> = ({ message, onConfirm }) => {
   return (
-    <Overlay>
-      <ModalContainer>
-        <Title>알림</Title>
-        <Message>{message}</Message>
-        <Divider />
-        <ConfirmButton onClick={onConfirm}>확인</ConfirmButton>
-      </ModalContainer>
-    </Overlay>
+    <L.Overlay>
+      <L.ModalContainer>
+        <L.Title>알림</L.Title>
+        <L.Message>{message}</L.Message>
+        <L.Divider />
+        <L.ConfirmButton onClick={onConfirm}>확인</L.ConfirmButton>
+      </L.ModalContainer>
+    </L.Overlay>
   )
 }
 
--- frontend/src/components/AlertPopUp/AlertPopUp2/AlertPopUp2.tsx
+++ frontend/src/components/AlertPopUp/AlertPopUp2/AlertPopUp2.tsx
@@ -1,78 +1,5 @@
 import React from 'react'
-import styled from 'styled-components'
-
-const Overlay = styled.div`
-  position: fixed;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  background-color: rgba(0, 0, 0, 0.5);
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  z-index: 999;
-`
-
-const ModalContainer = styled.div`
-  background-color: white;
-  border-radius: 10px;
-  width: 278px;
-  text-align: center;
-  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
-  position: relative;
-`
-
-const Title = styled.h2`
-  margin: 0;
-  padding-top: 20px;
-  padding-bottom: 5px;
-  font-size: 18px;
-  font-weight: bold;
-  text-align: center;
-`
-
-const Message = styled.p`
-  margin: 0;
-  padding: 30px;
-  line-height: 1.5;
-  font-size: 14px;
-  text-align: center;
-`
-
-const Divider = styled.hr`
-  border: none;
-  border-top: 1px solid #525fd4;
-  margin: 0;
-`
-
-const ButtonContainer = styled.div`
-  width: 100%;
-  height: 50px; /* 높이를 50px로 설정 */
-`
-
-const Button1 = styled.button`
-  float: center;
-  background-color: white;
-  width: 45%;
-  height: 100%; /* 부모 컨테이너의 높이에 맞춤 */
-  border: 0;
-  padding-right: 15px;
-  font-size: 15px;
-  cursor: pointer;
-  border-right: 0.5px solid #525fd4;
-`
-const Button2 = styled.button`
-  float: center;
-  background-color: white;
-  width: 45%;
-  height: 100%; /* 부모 컨테이너의 높이에 맞춤 */
-  border: 0;
-  padding-left: 15px;
-  font-size: 15px;
-  cursor: pointer;
-  border-left: 0.5px solid #525fd4;
-`
+import * as L from './AlertPopUp2.styles/AlerPopUp2.style' // 스타일 컴포넌트 임포트
 
 // Props 타입 정의
 interface AlertPopUp2Props {
@@ -89,19 +16,19 @@ const AlertPopUp2: React.FC<AlertPopUp2Props> = ({
   return (
     <>
       <div className='popup_dimmed'></div> {/* 팝업 백그라운드 */}
-      <Overlay>
-        <ModalContainer>
-          <Title>알림</Title>
-          <Message>{message}</Message>
-          <Divider />
-          <ButtonContainer>
-            <Button1 className='no' onClick={onCancel}>
+      <L.Overlay>
+        <L.ModalContainer>
+          <L.Title>알림</L.Title>
+          <L.Message>{message}</L.Message>
+          <L.Divider />
+          <L.ButtonContainer>
+            <L.Button1 className='no' onClick={onCancel}>
               아니요
-            </Button1>
-            <Button2 onClick={onConfirm}>네</Button2>
-          </ButtonContainer>
-        </ModalContainer>
-      </Overlay>
+            </L.Button1>
+            <L.Button2 onClick={onConfirm}>네</L.Button2>
+          </L.ButtonContainer>
+        </L.ModalContainer>
+      </L.Overlay>
     </>
   )
 }
