68ca5fdfb345d9c58299d6d3da4d9ea7e1c68e77
jiheunkim <ziheun54@gmail.com>
2024-09-15 22:51:31 +0900

    [Feat 💡] 캘린더 내 장소추가 기능 구현

--- frontend/src/pages/DateSelectedPage/DateSelected.tsx
+++ frontend/src/pages/DateSelectedPage/DateSelected.tsx
@@ -1,7 +1,106 @@
+import React, { useState, useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+import { useSwipeable } from 'react-swipeable'
+
+import MainCalendar from './components/MainCalendar'
+import * as L from './styles/DateSelected.style'
+import BackButton from '../../components/BackButton/BackButton'
+
 const DateSelected = () => {
+  const { contentid } = useParams<{ contentid: string }>()
+  const { place } = useParams<{ place: string }>()
+  const today = new Date()
+  const [currentDate, setCurrentDate] = useState({
+    year: today.getFullYear(),
+    month: today.getMonth() + 1,
+  })
+
+  useEffect(() => {
+    setCurrentDate({ year: today.getFullYear(), month: today.getMonth() + 1 })
+  }, [])
+
+  const handleYearChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setCurrentDate({ ...currentDate, year: Number(event.target.value) })
+  }
+
+  const handleMonthChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setCurrentDate({ ...currentDate, month: Number(event.target.value) })
+  }
+
+  const generateYearOptions = () => {
+    const years = []
+    for (let i = 2024; i <= today.getFullYear() + 1; i++) {
+      years.push(
+        <option key={i} value={i}>
+          {i}
+        </option>,
+      )
+    }
+    return years
+  }
+
+  const generateMonthOptions = () => {
+    const months = []
+    for (let i = 1; i <= 12; i++) {
+      months.push(
+        <option key={i} value={i}>
+          {i}
+        </option>,
+      )
+    }
+    return months
+  }
+
+  const handleSwipeLeft = () => {
+    if (currentDate.month === 12) {
+      setCurrentDate({ year: currentDate.year + 1, month: 1 })
+    } else {
+      setCurrentDate({ year: currentDate.year, month: currentDate.month + 1 })
+    }
+  }
+
+  const handleSwipeRight = () => {
+    if (currentDate.month === 1) {
+      setCurrentDate({ year: currentDate.year - 1, month: 12 })
+    } else {
+      setCurrentDate({ year: currentDate.year, month: currentDate.month - 1 })
+    }
+  }
+
+  const handlers = useSwipeable({
+    onSwipedLeft: handleSwipeLeft,
+    onSwipedRight: handleSwipeRight,
+    trackMouse: true,
+    delta: 10,
+  })
+
   return (
-    <div>
-      <p>DateSelected</p>
+    <div {...handlers}>
+      <L.HeaderSection>
+        <BackButton />
+        <L.HeaderTitle>
+          <L.CalendarSelect
+            value={currentDate.year}
+            onChange={handleYearChange}
+          >
+            {generateYearOptions()}
+          </L.CalendarSelect>
+          년&nbsp;&nbsp;
+          <L.CalendarSelect
+            value={currentDate.month}
+            onChange={handleMonthChange}
+          >
+            {generateMonthOptions()}
+          </L.CalendarSelect>
+          월
+        </L.HeaderTitle>
+      </L.HeaderSection>
+      <MainCalendar
+        year={currentDate.year}
+        month={currentDate.month}
+        contentid={contentid!}
+        place={place!}
+      />
     </div>
   )
 }
---
+++ frontend/src/pages/DateSelectedPage/styles/PlaceAddBanner.style.tsx
@@ -0,0 +1,41 @@
+import { styled } from 'styled-components'
+
+export const BannerSection = styled.div`
+  padding: 1rem;
+`
+
+export const Banner = styled.div`
+  background-color: #eff1ff;
+  border-radius: 0.5rem;
+`
+
+export const BannerContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding: 1rem;
+`
+
+export const BannerText = styled.p`
+  font-size: 0.9rem;
+`
+
+export const BannerDateText = styled.p`
+  font-size: 1.1rem;
+  margin-top: 0.5rem;
+`
+
+export const ButtonContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
+
+export const SelectedButton = styled.button`
+  padding: 0.3rem 1.1rem;
+  border: none;
+  border-radius: 1.5rem;
+  background-color: #525fd4;
+  color: white;
+  font-size: 0.9rem;
+  font-weight: 600;
+`
