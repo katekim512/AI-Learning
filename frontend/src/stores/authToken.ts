fabb23f4a84f9a81c9bffb1147f09b284f5b8658
jiheunkim <ziheun54@gmail.com>
2024-09-17 01:45:29 +0900

    [Test 💻] 카카오 로그인 로직 수정에 따른 변경

--- frontend/src/stores/authToken.ts
+++ frontend/src/stores/authToken.ts
@@ -1,72 +1,41 @@
 class AuthToken {
-  private accessToken: string
-  private refreshToken: string
-  private readonly TOKEN_KEY: string = 'authTokens' // 저장할 키 값
+  private token: string
+  private readonly TOKEN_KEY: string = 'authToken' // 저장할 키 값
 
   constructor() {
     // 초기화 시 localStorage에서 토큰 정보를 가져옵니다.
-    const storedTokens = localStorage.getItem(this.TOKEN_KEY)
-    if (storedTokens) {
-      const parsedTokens = JSON.parse(storedTokens)
-      this.accessToken = parsedTokens.accessToken || ''
-      this.refreshToken = parsedTokens.refreshToken || ''
+    const storedToken = localStorage.getItem(this.TOKEN_KEY)
+    if (storedToken) {
+      this.token = storedToken || ''
     } else {
-      this.accessToken = ''
-      this.refreshToken = ''
+      this.token = ''
     }
   }
 
-  // 새로운 accessToken과 refreshToken을 설정하고 저장합니다.
-  setTokens(newAccessToken: string, newRefreshToken: string) {
+  // 새로운 accessToken을 설정하고 저장합니다.
+  setToken(newAccessToken: string) {
     try {
-      const tokenData = {
-        accessToken: newAccessToken,
-        refreshToken: newRefreshToken,
-      }
-      localStorage.setItem(this.TOKEN_KEY, JSON.stringify(tokenData))
-      this.accessToken = newAccessToken
-      this.refreshToken = newRefreshToken
+      localStorage.setItem(this.TOKEN_KEY, newAccessToken)
+      this.token = newAccessToken
     } catch (e) {
-      this.accessToken = ''
-      this.refreshToken = ''
+      this.token = ''
     }
   }
 
   // accessToken을 가져오는 메서드
   getAccessToken() {
-    return this.accessToken
+    return this.token
   }
 
-  // refreshToken을 가져오는 메서드
-  getRefreshToken() {
-    return this.refreshToken
-  }
-
-  // 특정 토큰만 설정하는 경우
-  setAccessToken(newAccessToken: string) {
-    this.accessToken = newAccessToken
-    this.updateLocalStorage()
-  }
-
-  setRefreshToken(newRefreshToken: string) {
-    this.refreshToken = newRefreshToken
-    this.updateLocalStorage()
-  }
-
-  // 로컬스토리지에 저장된 토큰 정보를 업데이트합니다.
+  // accessToken을 로컬스토리지에 저장된 정보로 업데이트합니다.
   private updateLocalStorage() {
-    const tokenData = {
-      accessToken: this.accessToken,
-      refreshToken: this.refreshToken,
-    }
-    localStorage.setItem(this.TOKEN_KEY, JSON.stringify(tokenData))
+    localStorage.setItem(this.TOKEN_KEY, this.token)
   }
 
   // 토큰 정보를 제거합니다.
-  removeTokens() {
+  removeToken() {
     localStorage.removeItem(this.TOKEN_KEY)
-    this.accessToken = ''
-    this.refreshToken = ''
+    this.token = ''
   }
 }
 
