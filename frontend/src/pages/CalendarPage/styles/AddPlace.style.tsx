2118531d930038558b1d03d079ff25956d89d068
YeeunKim <katekim512@konkuk.ac.kr>
2024-09-18 03:58:55 +0900

    [Fix✏️] PlaceNumber 오른쪽 정렬

--- frontend/src/pages/CalendarPage/styles/AddPlace.style.tsx
+++ frontend/src/pages/CalendarPage/styles/AddPlace.style.tsx
@@ -68,13 +68,17 @@ export const PlaceItem = styled.li`
   display: flex;
   align-items: center;
   padding: 10px 0;
+  position: relative; /* 자식 요소의 절대 위치를 위한 상대 위치 */
 `
 
 export const PlaceNumber = styled.span`
   font-size: 12px;
   font-weight: bold;
-  margin-left: 5px;
-  margin-right: 15px;
+  width: 20px; /* 고정된 너비로 모든 번호를 일관되게 표시 */
+  text-align: right; /* 텍스트를 오른쪽 정렬 */
+  margin-right: 20px; /* 번호와 이미지 사이의 간격 추가 */
+  display: inline-block; /* 너비와 텍스트 정렬을 적용하기 위해 사용 */
+  flex-shrink: 0; /* PlaceNumber의 크기를 줄이지 않도록 설정 */
 `
 
 export const PlaceImage = styled.img`
