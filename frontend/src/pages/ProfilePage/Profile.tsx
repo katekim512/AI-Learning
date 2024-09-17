d6cadcc81c56cd2f99f9af7b963430bb94346edb
jiheunkim <ziheun54@gmail.com>
2024-09-18 03:38:39 +0900

    [Design 🎨] 교육패턴 관련 배너 붙이기

--- frontend/src/pages/ProfilePage/Profile.tsx
+++ frontend/src/pages/ProfilePage/Profile.tsx
@@ -1,13 +1,27 @@
+import { useState } from 'react'
+
 import MyLevel from './components/MyLevel'
 import ProfileSection from './components/ProfileSection'
 import * as L from './styles/Profile.style'
+import InfoBanner from '../../components/InfoBanner/InfoBanner'
 
 const Profile = () => {
+  const [showBanner, setShowBanner] = useState(true)
+  const bannerText = '나의 교육 여행 패턴 및 방향성을 알고 싶나요?'
+
   return (
     <>
       <L.Container>
         <ProfileSection />
         <MyLevel />
+        <L.BannerContainer>
+          {showBanner && (
+            <InfoBanner
+              text={bannerText}
+              onClose={() => setShowBanner(false)}
+            />
+          )}
+        </L.BannerContainer>
       </L.Container>
     </>
   )
