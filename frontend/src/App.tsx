191f25f22dfbbaec9a55ce5f8be904d5ae7aa17f
jiheunkim <ziheun54@gmail.com>
2024-09-19 05:16:08 +0900

    [Test 💻] 로그인 이후 전체 목록 받아오게끔 수정

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,27 @@
 import { useEffect } from 'react'
-import { Outlet, useLocation } from 'react-router-dom'
+import { Outlet, useLocation, useNavigate } from 'react-router-dom'
 
 import BottomMenuBar from './components/BottomMenuBar/BottomMenuBar'
 import Loading from './components/Loading/Loading'
 import { useAllPlace } from './hooks/useAllPlace'
+import authToken from './stores/authToken'
 import GlobalStyle from './style/GlobalStyle'
 import { MobileContainer } from './style/MobileStyle'
 import setScreenHeight from './utils/mobileScreenSize/setScreenHeight'
 
 const App = () => {
   const location = useLocation()
-  const { isLoading } = useAllPlace()
+  const navigate = useNavigate()
+  const token = authToken.getAccessToken()
+
+  useEffect(() => {
+    if (!token) {
+      navigate('/login')
+    }
+  }, [token, navigate])
+
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  const { isLoading } = token ? useAllPlace() : { isLoading: false }
 
   useEffect(() => {
     setScreenHeight()
