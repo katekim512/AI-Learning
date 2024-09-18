dcfa8c85adc3100cd4cbc3a92e858411e874effe
jiheunkim <ziheun54@gmail.com>
2024-09-19 04:29:24 +0900

    [Test 💻] 모든 api 401에러 처리

--- frontend/src/api/axiosInstance.ts
+++ frontend/src/api/axiosInstance.ts
@@ -1,4 +1,7 @@
 import axios from 'axios'
+import { useNavigate } from 'react-router-dom'
+
+import { isAxios401Error } from '../utils/isAxios401Error'
 
 const url = process.env.REACT_APP_API_URL
 
@@ -10,3 +13,19 @@ export const aiLearningAxios = axios.create({
   baseURL: url,
   withCredentials: true,
 })
+
+export const setupAxiosInterceptors = () => {
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  const navigate = useNavigate()
+
+  aiLearningAxios.interceptors.response.use(
+    response => response, // 성공적인 응답은 그대로 반환
+    error => {
+      if (isAxios401Error(error)) {
+        // 401 에러 발생 시 '/'로 이동
+        navigate('/')
+      }
+      return Promise.reject(error)
+    },
+  )
+}
