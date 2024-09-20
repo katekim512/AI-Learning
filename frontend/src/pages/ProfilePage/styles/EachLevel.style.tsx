8d4e4d53a648a2178828c190d42790672900cc1d
YeeunKim <katekim512@konkuk.ac.kr>
2024-09-21 04:47:15 +0900

    [Design🎨] LevelPage UI 구축

---
+++ frontend/src/pages/ProfilePage/styles/EachLevel.style.tsx
@@ -0,0 +1,74 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+`
+export const ScrollContainer = styled.div`
+  height: calc(100vh - 8rem);
+  overflow-y: auto;
+`
+export const ButtonContainer = styled.div`
+  padding: 20px;
+`
+
+export const Section = styled.div`
+  margin-bottom: 40px;
+`
+
+export const Title = styled.h2`
+  font-size: 22px;
+  color: #333;
+  margin: 0px 20px;
+  margin-bottom: 20px;
+`
+
+export const SectionHeader = styled.h2`
+  font-size: 16px;
+  color: #525fd4;
+  border-bottom: 2px solid #007aff;
+  padding: 10px 20px;
+`
+
+export const DestinationList = styled.ul`
+  list-style: none;
+  //padding: 0px 0px;
+`
+
+export const DestinationItem = styled.li<{ visited?: boolean }>`
+  display: flex;
+  align-items: center;
+  padding: 10px 20px;
+  border-bottom: 1px solid #eaeaea;
+  background-color: ${({ visited }) =>
+    visited ? '#f0f0f0' : 'white'}; /* Gray background if visited */
+  color: ${({ visited }) =>
+    visited ? '#a9a9a9' : '#333'}; /* Lighter text color if visited */
+`
+
+export const Image = styled.img`
+  width: 60px;
+  height: 60px;
+  border-radius: 8px;
+  object-fit: cover;
+  margin-right: 20px;
+`
+
+export const TextContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`
+
+export const Name = styled.span`
+  font-size: 16px;
+  font-weight: bold;
+  color: #333;
+  margin-bottom: 3px;
+`
+
+export const Location = styled.span`
+  font-size: 12px;
+  color: #666;
+  margin-top: 3px;
+`
