3624088eeb3bd4e7c57eff7fa2ff3f74f4e50968
YeeunKim <katekim512@konkuk.ac.kr>
2024-09-25 20:26:36 +0900

    [Fix✏️] 아이콘 불러오는 방식 변경

--- frontend/src/pages/AISchedule3Page/AISchedule3.tsx
+++ frontend/src/pages/AISchedule3Page/AISchedule3.tsx
@@ -1,4 +1,4 @@
-// import { Icon } from '@iconify/react'
+import { Icon } from '@iconify/react'
 // import peopleChatIcon from '@iconify-icons/fluent/people-chat-16-filled'
 import { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
@@ -16,27 +16,27 @@ const AISchedule3 = () => {
   const navigate = useNavigate()
   const [scheduleInfo, setScheduleInfo] = useState<AISchedule[]>([])
 
-  const GradientIcon = () => (
-    <svg
-      width='24'
-      height='24'
-      viewBox='0 0 24 24'
-      fill='none'
-      xmlns='http://www.w3.org/2000/svg'
-      style={{ marginRight: '5px' }}
-    >
-      <defs>
-        <linearGradient id='gradient' x1='0%' y1='0%' x2='100%' y2='0%'>
-          <stop offset='0%' stopColor='#00c6ff' />
-          <stop offset='100%' stopColor='#0072ff' />
-        </linearGradient>
-      </defs>
-      <path
-        d='M12 3C6.5 3 2 6.58 2 11C2.05 13.15 3.06 15.17 4.75 16.5C4.75 17.1 4.33 18.67 2 21C4.37 20.89 6.64 20 8.47 18.5C9.61 18.83 10.81 19 12 19C17.5 19 22 15.42 22 11C22 6.58 17.5 3 12 3ZM12 17C7.58 17 4 14.31 4 11C4 7.69 7.58 5 12 5C16.42 5 20 7.69 20 11C20 14.31 16.42 17 12 17Z'
-        fill='url(#gradient)'
-      />
-    </svg>
-  )
+  // const GradientIcon = () => (
+  //   <svg
+  //     width='24'
+  //     height='24'
+  //     viewBox='0 0 24 24'
+  //     fill='none'
+  //     xmlns='http://www.w3.org/2000/svg'
+  //     style={{ marginRight: '5px' }}
+  //   >
+  //     <defs>
+  //       <linearGradient id='gradient' x1='0%' y1='0%' x2='100%' y2='0%'>
+  //         <stop offset='0%' stopColor='#00c6ff' />
+  //         <stop offset='100%' stopColor='#0072ff' />
+  //       </linearGradient>
+  //     </defs>
+  //     <path
+  //       d='M12 3C6.5 3 2 6.58 2 11C2.05 13.15 3.06 15.17 4.75 16.5C4.75 17.1 4.33 18.67 2 21C4.37 20.89 6.64 20 8.47 18.5C9.61 18.83 10.81 19 12 19C17.5 19 22 15.42 22 11C22 6.58 17.5 3 12 3ZM12 17C7.58 17 4 14.31 4 11C4 7.69 7.58 5 12 5C16.42 5 20 7.69 20 11C20 14.31 16.42 17 12 17Z'
+  //       fill='url(#gradient)'
+  //     />
+  //   </svg>
+  // )
 
   const dummyScheduleData = [
     {
@@ -199,7 +199,7 @@ const AISchedule3 = () => {
             handleDelete={handleDelete}
           />
           <L.GuideRequestButton onClick={() => handleGuideRequest()}>
-            <GradientIcon />
+            <Icon icon='fluent:people-chat-16-filled' width='24' height='24' />
             해당 일정으로 가이드를 구하고 싶나요?
           </L.GuideRequestButton>
         </L.Container>
--- frontend/src/pages/AISchedule3Page/styles/AISchedule3.style.tsx
+++ frontend/src/pages/AISchedule3Page/styles/AISchedule3.style.tsx
@@ -209,12 +209,12 @@ export const MinusCircleIcon = styled.div`
   background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24'%3E%3Cpath fill='%23d20000' d='M12 2C6.486 2 2 6.486 2 12s4.486 10 10 10s10-4.486 10-10S17.514 2 12 2m5 11H7v-2h10z'/%3E%3C/svg%3E");
   margin-right: 8px; /* Space between icon and text */
 `
-export const GradientIcon = styled.span`
-  background: linear-gradient(to right, #00c6ff, #0072ff);
-  -webkit-background-clip: text;
-  -webkit-text-fill-color: transparent;
-  display: inline-block;
-`
+// export const GradientIcon = styled.span`
+//   background: linear-gradient(to right, #00c6ff, #0072ff);
+//   -webkit-background-clip: text;
+//   -webkit-text-fill-color: transparent;
+//   display: inline-block;
+// `
 
 export const GuideRequestButton = styled.button`
   padding: 0 0 0 5px; /* top, right, bottom, left */
