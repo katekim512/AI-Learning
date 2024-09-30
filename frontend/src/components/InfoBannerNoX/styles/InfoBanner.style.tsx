231e74e4bc3a018a3ca2eaa676888f176746e58e
YeeunKim <katekim512@konkuk.ac.kr>
2024-09-30 14:39:05 +0900

    [Fix✏️] 배너 고정 및 X 삭제

---
+++ frontend/src/components/InfoBannerNoX/styles/InfoBanner.style.tsx
@@ -0,0 +1,43 @@
+import styled from 'styled-components'
+
+export const BannerContainer = styled.div`
+  background-color: #eff1ff; /* 라이트 퍼플 배경 */
+  color: black;
+  padding: 10px;
+  border-radius: 5px;
+  text-align: left;
+  margin-bottom: 15px;
+  position: relative;
+  margin-left: 10px;
+  margin-right: 10px;
+`
+
+export const BannerText = styled.p`
+  margin: 0;
+  font-size: 11px;
+  padding: 0.2rem;
+  margin-left: 10px;
+  margin-bottom: 5px;
+`
+export const CloseButtonContainer = styled.button`
+  position: absolute;
+  top: 5px;
+  right: 3px;
+  height: 14px;
+  background: none;
+  text-align: right;
+  border: none;
+  width: 100%;
+  cursor: pointer;
+`
+
+export const CloseButton = styled.button`
+  position: absolute;
+
+  top: 5px;
+  right: 10px;
+  background: none;
+  border: none;
+  font-size: 9px;
+  cursor: pointer;
+`
