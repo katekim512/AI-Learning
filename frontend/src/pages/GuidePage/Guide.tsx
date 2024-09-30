ed98b6a57166b6fa5d522ca274d3bb1e3d0645fb
jiheunkim <ziheun54@gmail.com>
2024-10-01 01:22:24 +0900

    [Feat 💡] 가이드 목록 구현

--- frontend/src/pages/GuidePage/Guide.tsx
+++ frontend/src/pages/GuidePage/Guide.tsx
@@ -1,19 +1,33 @@
 import React from 'react'
 
+import GuideBox from './components/GuideBox'
 import * as L from './styles/Guide.style'
+// import { getPattern } from '../../api/profile/getPattern'
+import { postMessage } from '../../api/guide/postMessage'
 import { useUser } from '../../hooks/useUser'
+import authToken from '../../stores/authToken'
 
 const Guide: React.FC = () => {
+  const token = authToken.getAccessToken()
   const { data: userInfo } = useUser()
+
+  const handleAPITest = async () => {
+    if (token) {
+      const successResponse = await postMessage(token, 1234)
+      console.log(successResponse?.data)
+    }
+  }
 
   return (
     <L.AppContainer>
       <L.Title>
         <h1>
-          <L.Nickname>{userInfo?.nickname}</L.Nickname>님 일정에 어울리는
+          <L.Nickname onClick={handleAPITest}>{userInfo?.nickname}</L.Nickname>
+          님 일정에 어울리는
           <br></br>가이드 분들이에요!
         </h1>
       </L.Title>
+      <GuideBox />
     </L.AppContainer>
   )
 }
