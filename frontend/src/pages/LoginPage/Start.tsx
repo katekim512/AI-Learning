3c3209aa8f6e082a69c21b3a57fbb31657598bae
YeeunKim <katekim512@konkuk.ac.kr>
2024-10-13 15:02:32 +0900

    ts + css 해결

--- frontend/src/pages/LoginPage/Start.tsx
+++ frontend/src/pages/LoginPage/Start.tsx
@@ -1,7 +1,28 @@
+import React, { useEffect } from 'react'
+
 import StartLoginForm from './components/StartLoginForm'
 import * as L from './styles/Start.style'
 
 const Start = () => {
+  let vh = 0
+
+  useEffect(() => {
+    vh = window.innerHeight * 0.01
+    document.documentElement.style.setProperty('--vh', `${vh}px`)
+
+    const setVh = () => {
+      document.documentElement.style.setProperty(
+        '--vh',
+        `${window.innerHeight * 0.01}px`,
+      )
+    }
+
+    window.addEventListener('resize', setVh)
+    setVh()
+
+    return () => window.removeEventListener('resize', setVh)
+  }, [])
+
   return (
     <>
       <L.Container>
--- frontend/src/pages/LoginPage/styles/Start.style.tsx
+++ frontend/src/pages/LoginPage/styles/Start.style.tsx
@@ -3,7 +3,7 @@ import styled from 'styled-components'
 export const Container = styled.div`
   box-sizing: border-box;
   width: 100%;
-  height: 100vh;
+  height: calc(var(--vh, 1vh) * 100);
   display: flex;
   flex-direction: column;
   background-color: #525fd4;
